2eca1150e48ad05ddb2f39adeafbc48c8f9fe90a
nogihyeon <kopmor6312@naver.com>
2020-05-12 20:47:08 +0900

    Initial commit

---
+++ app/src/main/java/com/pnu/whatyoueat/Fragment1.java
@@ -0,0 +1,21 @@
+package com.pnu.whatyoueat;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class Fragment1 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment1, container, false);
+
+        initUI(rootView);
+
+        return rootView;
+    }
+    private void initUI(ViewGroup rootView){}
+}
